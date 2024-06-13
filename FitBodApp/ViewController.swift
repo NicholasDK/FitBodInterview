b5bdca9989f76aa9ffbf060f80b7548927a13e76
Nicholas Della Valle <nickdv125@gmail.com>
2024-06-13 13:32:38 -0400

    Initial Commit

---
+++ FitBodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitBodApp
+//
+//  Created by Nicholas Della Valle on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
